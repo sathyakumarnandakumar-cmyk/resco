7e6b72e479bcd0a934dafbf0efdbd9a18bdd5324
Michał Witas <michasiek098@gmail.com>
2023-06-15 21:15:58 +0200

    Functional API

--- resco_benchmark/agents/nets.py
+++ resco_benchmark/agents/nets.py
@@ -1,6 +1,9 @@
 import torch.nn as nn
 from pfrl.q_functions import DiscreteActionValueHead
 
+from agents.models.default import DefaultModel
+from agents.models.activations.swish import SwishActivation
+
 
 def get_net(net, obs_space, act_space, h, w):
     nets = {
@@ -50,5 +53,10 @@ def get_net(net, obs_space, act_space, h, w):
             nn.Linear(64, act_space),
             DiscreteActionValueHead(),
         ),
+        "default_swish": DefaultModel(obs_space=obs_space,
+                            act_space=act_space,
+                            h=h,
+                            w=w,
+                            acti=SwishActivation)
     }
     return nets[net]
