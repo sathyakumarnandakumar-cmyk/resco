f55e00acdc96bcc1d0469cc466e8372271328356
Maciej Szymczyk <maciej.szymczyk06@gmail.com>
2023-08-07 21:30:05 +0200

    Added script for parallel experiments

---
+++ resco_benchmark/parallel_experiments.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+agents=("IDQN" "IPPO")
+activations=("relu" "tanh")
+nets=("default" "double_conv")
+seeds=(1 2)
+for agent in ${agents[@]}; do
+    if [ $agent == "IDQN" ]
+    then
+        for activation in ${activations[@]}; do
+            for net in ${nets[@]}; do
+                for seed in ${seeds[@]}; do
+                    session_name="$agent""-""$activation""-""$net""-""$seed"
+                    tmux new-session -d -s $session_name
+                    tmux send-keys -t "$session_name".0 'python3 main.py --agent '$agent' --map BB5B --net '$net' --activation '$activation' --seed '$seed'' ENTER
+                                done
+                        done
+                done
+    else
+        session_name="$agent"
+        tmux new-session -d -s $session_name
+        tmux send-keys -t "$session_name".0 "python3 main.py --agent "$agent" --map BB5B" ENTER
+    fi
+done
+
+
