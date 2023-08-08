0b794f42428b7393ef2c06c54f410602db661495
maciej.szymczyk06 <maciej.szymczyk06@gmail.com>
2023-08-08 15:18:38 +0200

    corrected a mistake related to functional api

--- resco_benchmark/agents/nets.py
+++ resco_benchmark/agents/nets.py
@@ -2,14 +2,14 @@ import torch.nn as nn
 from agents.models.default import DefaultModel
 from agents.models.double_conv import DoubleConv
 from agents.models.calculate_output_size import conv2d_size_out
-
+from agents.models.activations.swish import swish
 
 def get_net(net, activation,    obs_space, act_space, h, w):
     activations = {
-        'relu': nn.ReLU,
-        'tanh': nn.Tanh,
-        'leaky_relu': nn.LeakyReLU,
-        'swish': nn.SiLU
+        'relu': nn.functional.relu,
+        'tanh': nn.functional.tanh,
+        'leaky_relu': nn.functional.leaky_relu,
+        'swish': swish
     }
     h2 = conv2d_size_out(h)
     w2 = conv2d_size_out(w)
