c9a5ae181a0ab35e30422563e5a9ac4249333191
Adam Cichy <adam_cichy@outlook.com>
2025-02-10 23:11:21 +0100

    chore: improve profiling scripts

--- resco_benchmark/cprofile.sh
+++ resco_benchmark/cprofile.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+mkdir -p profiling
 filename=$(date +"%Y_%m_%d_%H%M%S")
-python -m cProfile -s cumulative -o profiling/$filename.stats $*
+python -m cProfile -s cumulative -o profiling/"$filename".stats "$*"
--- resco_benchmark/profile_scalene.sh
+++
@@ -1,4 +0,0 @@
-set -o allexport
-source .env
-set +o allexport
-python -m scalene --html --outfile scalene.html main.py --agent IDQN --map BB5B --eps 3
